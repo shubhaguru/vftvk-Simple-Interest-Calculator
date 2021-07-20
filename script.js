ee72f8cbe9d5fcc59a921bd847dbb46d0beb849b
shubhaguru <shubhan.rao@gmail.com>
2021-07-20 16:43:50 +0000

    Add SI calculator

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var ri = document.getElementById("rate").value;
+    var yrs = document.getElementById("years").value;
+    var amount = p*yrs*rate/100;
+    var years = new Date().getFullYear()+parseInt(yrs);
+    document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+ri+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+years+"\<br\>"
 }
         
+function updateRate()
+{
+    // Get the rate val from slider and update the html field
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val") = rateVal;
+}
