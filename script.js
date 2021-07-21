1fd469264e067cced53d997a9aabef68a9f3a4df
shubhaguru <shubhan.rao@gmail.com>
2021-07-21 17:07:46 +0000

    Fix JS

--- script.js
+++ script.js
@@ -6,25 +6,14 @@ function compute()
         document.getElementById("principal").focus();
     }
 
-    if (typeof p == 'number') alert(p+" is num");
-
     var ri = document.getElementById("rate").value;
-    if (typeof ri == 'number') alert(ri+" is num");
-
     var yrs = document.getElementById("years").value;
-   if (typeof yrs == 'number') alert(yrs+" is num");
-    var interest = (p * yrs * rate)/100;
+    var interest = (p * yrs * ri)/100;
     var year = new Date().getFullYear()+parseInt(yrs);
     document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+ri+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
 
 
-if(typeof num == 'number'){
-        alert(num + " is a number");
-       }else{
-        alert(num + " is not a number");
-       }
-        
 function updateRate()
 {
     // Get the rate val from slider and update the html field
